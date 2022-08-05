a36c1bf85867a87e916aa60971555286fd16b91c
Sean Chalmers <sean.chalmers@obsidian.systems>
2022-08-05 14:21:56 +0100

    Add default.nix, but terrible

---
+++ default.nix
@@ -0,0 +1,38 @@
+{ stdenv
+, lib
+, enModel
+, xdotool
+, python3
+, py-vosk
+}:
+python3.pkgs.buildPythonApplication rec {
+  pname = "nerd-dictation";
+  version = "20220605";
+  format = "other";
+  src = ./.;
+
+  VOSK_MODEL_PATH = "${enModel}";
+
+  propagatedBuildInputs = with python3.pkgs; [
+    xdotool
+    py-vosk
+    requests
+    tqdm
+    srt
+    websockets
+  ];
+
+  phases = "installPhase";
+
+  # Not entirely sure why I had to do this manually...
+  makeWrapperArgs = [
+    "--prefix PYTHONPATH ':' ${python3}/${python3.sitePackages}/"
+    "--prefix LD_LIBRARY_PATH ':' ${lib.makeLibraryPath [stdenv.cc.cc.lib]}"
+  ];
+
+  installPhase = ''
+    mkdir -p $out/bin
+    cp $src/nerd-dictation $out/bin/nerd-dictation
+    wrapPythonProgramsIn $out/bin $PYTHONPATH
+  '';
+}
